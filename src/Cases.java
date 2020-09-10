021176f0ed4d9edd9a9324431f385a65083c6912
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-09-10 13:16:38 +0200

    Ny kode

--- src/Cases.java
+++ src/Cases.java
@@ -1,6 +1,15 @@
 class selektion_1{
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 1;
+        int z = 0;
 
-
+        System.out.println(x <= 5 && z != y);
+        System.out.println(x == 5 || x == y && z == 3);
+        System.out.println(x / y > z / x);
+        System.out.println(!( x != y - z ) == false);
+        System.out.println(2 * x != x || x == 0);
+        System.out.println(! true || ! false);
 }
 
 class selektion_2{
